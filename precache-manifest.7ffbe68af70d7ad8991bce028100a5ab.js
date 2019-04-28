2ccb45b2ada6c5ebe36600d6bb5acc1e0b71b7d4
George <bl1723r@gmail.com>
2019-04-28 19:41:17 +0300

    Updates

---
+++ precache-manifest.7ffbe68af70d7ad8991bce028100a5ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28953a9530c3c7744e5e8587cccceb81",
+    "url": "/trollify/index.html"
+  },
+  {
+    "revision": "edc2d9c1230591ff2f77",
+    "url": "/trollify/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "e2ca52397e947877372e",
+    "url": "/trollify/static/css/main.8e6a3f60.chunk.css"
+  },
+  {
+    "revision": "edc2d9c1230591ff2f77",
+    "url": "/trollify/static/js/2.5b03823b.chunk.js"
+  },
+  {
+    "revision": "e2ca52397e947877372e",
+    "url": "/trollify/static/js/main.b74b1e98.chunk.js"
+  },
+  {
+    "revision": "0c0d23111b55cd453b93",
+    "url": "/trollify/static/js/runtime~main.3d23252b.js"
+  },
+  {
+    "revision": "bd866bc77e2c51bbf78bd7cce576c12a",
+    "url": "/trollify/static/media/logo.bd866bc7.png"
+  },
+  {
+    "revision": "8cc7beffa7f094297fda3bc6834478a1",
+    "url": "/trollify/static/media/trollface.8cc7beff.png"
+  }
+]);

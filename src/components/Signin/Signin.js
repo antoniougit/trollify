324897d54e07a7ee75868fe26e4710face932917
antoniougit <bl1723r@gmail.com>
2024-12-13 14:18:44 +0200

    minor fixes

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -93,13 +93,6 @@ class SignIn extends React.Component {
                                 value="Log in"
                             />
                         </div>
-                        {/* <div className="lh-copy mt3">
-                            <p
-                                onClick={() => onRouteChange('register')}
-                                className="f6 link dim black db pointer">
-                                Register
-                            </p>
-                        </div> */}
                     </div>
                 </main>
             </article>
